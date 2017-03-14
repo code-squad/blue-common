f8009545ca63acf75743e11e59f0ea97debe54aa
Hoyoung Jung <honux77@gmail.com>
2017-03-14 11:56:51 +0900

    day1 welcome

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+FILE=1-1.md
+
+if [ $# -eq 2 ]; then 
+	DIR = $2
+	FILE = $1
+else
+	num=($(ls | grep week | sort -r | head -1 | grep -o '[0-9]'))
+	DIRNAME=week$(expr $num + 1)
+fi
+
+cp -a template $DIRNAME
+mv $DIRNAME/sample.md $DIRNAME/$FILE
+
+echo "Done"
