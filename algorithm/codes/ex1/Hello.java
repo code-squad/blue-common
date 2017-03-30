258e690c5b5f132cb2ef1d0d989902a16f980b9c
Hoyoung Jung <honux77@gmail.com>
2017-03-31 01:32:15 +0900

    Update Java lecture note

---
+++ algorithm/codes/ex1/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	//entry point 
+	public static void main(String[] args) {
+		System.out.println("hello, world!");
+	}
+}
+
+/*
+ * javac Hello.java
+ * java Hello
+ */
---
+++ algorithm/codes/ex2/honux/Main.java
@@ -0,0 +1,7 @@
+package honux;
+public class Main {
+	public static void main(String[] args) {
+		//your code here
+		System.out.println("I am main");
+	}
+}
